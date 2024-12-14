8269315c67d7a0e9600fa82f5eaed1e571cddbfd
lytuan123 <130950679+lytuan123@users.noreply.github.com>
2024-12-14 20:55:59 +0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -9,7 +9,7 @@ def load_json(file_name: str) -> dict:
     with open(file_name, "r", encoding="utf-8") as f:
         return json.load(f)
 
-def save_json(file_name: str, data: Dict):
+def save_json(file_name: str, data: dict):
     with open(file_name, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
